796b4b8f399fe7306276d8ad19ba1135f09e8e2c
MAPI17RIOSNAVAS <riosnavasm@gmail.com>
2026-09-11 08:57:25 -0500

    feat:agregar variable entorno cors

--- src/identidad/usuarios/infraestructure/controllers/auth.controller.ts
+++ src/identidad/usuarios/infraestructure/controllers/auth.controller.ts
@@ -1,13 +1,26 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { LoginUseCase } from '../../aplication/use-cases/login.usecase';
 import { LoginDto } from '../../aplication/dto/login.dto';
+import { CreateUsuarioDto } from '../../aplication/dto/create-usuario.dto';
+import { CrearUsuarioUseCase } from '../../aplication/use-cases/crear-usuario.usecase';
 
-@Controller('auth')
+@Controller('auth') 
 export class AuthController {
-  constructor(private readonly loginUseCase: LoginUseCase) {}
+  CrearUsuarioUseCase: any;
+  constructor(
+    private readonly loginUseCase: LoginUseCase,
+    private readonly registerUseCase: CrearUsuarioUseCase,
+  ) {}
 
+  // Ruta: POST /api/auth/login
   @Post('login')
   async login(@Body() dto: LoginDto) {
     return this.loginUseCase.execute(dto.email, dto.password);
+  }
+
+  // Ruta: POST /api/auth/register 
+  @Post('register')
+  async register(@Body() dto: CreateUsuarioDto) {
+    return await this.CrearUsuarioUseCase.execute(dto);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,20 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN, 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true,          // elimina campos no declarados en el DTO
-      forbidNonWhitelisted: true, // rechaza el request si vienen campos extra
-      transform: true,           // convierte tipos automáticamente (ej. "5" → 5)
+      whitelist: true,       
+      forbidNonWhitelisted: true, 
+      transform: true,           
     }),
   );
 
-  app.enableCors(); // útil si el frontend corre en otro puerto/dominio
 
   const port = process.env.PORT ?? 3000;
   await app.listen(port);
